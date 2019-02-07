94870780cb95c85c6b1eac665d26b6e764bf4fde
Glyzewski, Stephanie <stephanie.glyzewski@gmail.com>
2019-02-06 19:07:09 -0600

    add test project to test ValuesController spec

---
+++ HubbardParkApi/HubbardParkApi.Tests/ValuesControllerTests.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using HubbardParkApi.Controllers;
+
+namespace HubbardParkApi.Tests
+{
+  public class ValuesControllerTests
+  {
+    [Fact]
+    public void GivenNoParameter_ShouldReturnHelloSpaceWorld()
+    {
+      var sut = new ValuesController();
+      var expected = "Hello space World";
+      var result = sut.Get();
+      Assert.Equal(expected, result.Value);
+    }
+  }
+}
