2460eb442d51aa0672f859141db2711226a349af
Glyzewski, Stephanie <stephanie.glyzewski@gmail.com>
2019-02-19 19:05:33 -0600

    Got test running by blowing away project

--- HubbardParkApi/HubbardParkApi.Tests/ValuesControllerTests.cs
+++ HubbardParkApi/HubbardParkApi.Tests/ValuesControllerTests.cs
@@ -1,19 +1,29 @@
-using System.Runtime.InteropServices.ComTypes;
-using HubbardParkApi.Controllers;
+using System;
+using AutoFixture;
+using AutoFixture.AutoMoq;
 using Xunit;
+using HubbardParkApi.Controllers;
 
 namespace HubbardParkApi.Tests
 {
   public class ValuesControllerTests
   {
-    
+    private readonly Fixture _fixture;
+    private readonly ValuesController _sut;
+
+    public ValuesControllerTests()
+    {
+      _fixture = new Fixture();
+      _fixture.Customize(new AutoMoqCustomization());
+      _sut = _fixture.Create<ValuesController>();
+    }
+
     [Fact]
     public void GivenNoParameter_ShouldReturnHelloSpaceWorld()
     {
-      //var sut = new ValuesController();
-      //var expected = "Hello space World";
-      //var result = sut.Get();
-      //Assert.Equal(expected, result.Value);
+      var expected = "Hello space World";
+      var result = _sut.Get();
+      Assert.Equal(expected, result.Value);
     }
   }
 }
