ce23c1b168204abc4e39b956ef6452947554fe29
Glyzewski, Stephanie <stephanie.glyzewski@gmail.com>
2019-02-19 19:59:05 -0600

    3 broken tests, http client imported and it doesn't really work...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DataService } from './data.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'where the rubber does not meet the rubber';
+
+
+constructor(private dataService: DataService) {
+}
+
+ngOnInit() {
+  this.dataService.getValues().subscribe(value => this.title = value);
+}
+
+
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class DataService {
+
+    private _url: string;
+
+    constructor(private _http: HttpClient) {
+        this._url = "https://localhost:44353/";
+    }
+    
+    getValues(): Observable<string> {
+        const url = this._url + 'api/Values/'
+        return this._http.get(url, {withCredentials: true, responseType: 'text'});
+    }
+}
